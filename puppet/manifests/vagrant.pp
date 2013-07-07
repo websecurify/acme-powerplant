f8121bcbde0806af4d5e350a704fd20ebf404a15
Petko D. Petkov <pdp@aluminum.home>
2013-07-07 16:24:19 +0100

    Code cleanup.

--- puppet/manifests/vagrant.pp
+++ puppet/manifests/vagrant.pp
@@ -1,47 +1,47 @@
 class system-update {
-  class { 'apt':
-  }
-
-  exec { 'apt-update':
-    command => '/usr/bin/apt-get update',
-  }
-
-  Exec['apt-update'] -> Package <| |>
+	class { 'apt':
+	}
+	
+	exec { 'apt-update':
+		command => '/usr/bin/apt-get update',
+	}
+	
+	Exec['apt-update'] -> Package <| |>
 }
 
 class mysql-setup {
-  require system-update
-
-  class { 'mysql':
-  }
-
-  class { 'mysql::php':
-  }
-
-  class { 'mysql::server':
-    config_hash => {
-      root_password => 'toor'
-    },
-  }
+	class { 'mysql':
+	}
+	
+	class { 'mysql::php':
+	}
+	
+	class { 'mysql::server':
+		config_hash => {
+			root_password => 'toor',
+		},
+	}
 }
 
 class apache-setup {
-  require system-update
+	class { 'apache':
+		mpm_module => 'prefork',
+	}
+	
+	class { 'apache::mod::php':
+	}
+	
+	apache::vhost { 'acme-powerplant':
+		port => '80',
+		docroot => '/vagrant/app',
+		default_vhost => true,
+	}
+}
 
-  class { 'apache':
-    mpm_module => 'prefork',
-  }
-
-  class { 'apache::mod::php':
-  }
-
-  apache::vhost { 'acme-powerplant':
-    port => '80',
-    docroot => '/vagrant/app',
-	default_vhost => true,
-  }
+class app-setup {
 }
 
 include system-update
 include mysql-setup
 include apache-setup
+include app-setup
